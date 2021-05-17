8ae7f85dcfa869e4265b53cfaa9ef4dbb9a79746
hardkov <krzysztofh150@interia.pl>
2021-05-17 22:08:54 +0200

    Changed components name

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 import streamlit as st
 
-import components.model as model_component
-import components.dataset as dataset_component
-import components.sidebar as sidebar_component
-import components.menu as menu_component
+import components.choose_model as choose_model
+import components.choose_dataset as choose_dataset
+import components.sidebar as sidebar
+import components.evaluation as evaluation
+import components.choose_task as choose_task
 
 apptitle = 'NLAPP'
 st.set_page_config(page_title=apptitle, page_icon=":cat")
 
-# state: task, model, dataset
-
-PAGES = { "Menu": menu_component, "Model": model_component, "Dataset": dataset_component }
-
 def main():
-  page_name = sidebar_component.write(PAGES)
-  page = PAGES[page_name]
-  page.write()
+  sidebar.write()
+
+  task = choose_task.write()
+  model = choose_model.write(task)
+  dataset = choose_dataset.write(task)
+
+  evaluation.write(task, model, dataset)
 
 if __name__ == "__main__":
   main()
