7d1f07fadb9c475b6eed5708bed3e6a010a82b99
hardkov <krzysztofH150@interia.pl>
2021-05-17 18:58:17 +0200

    Improved architecture

--- app.py
+++ app.py
@@ -3,20 +3,18 @@ import streamlit as st
 import components.model as model_component
 import components.dataset as dataset_component
 import components.sidebar as sidebar_component
+import components.menu as menu_component
 
 apptitle = 'NLAPP'
 st.set_page_config(page_title=apptitle, page_icon=":cat")
 
+# state: task, model, dataset
 
-PAGES = { "Model": model_component, "Dataset": dataset_component }
+PAGES = { "Menu": menu_component, "Model": model_component, "Dataset": dataset_component }
 
 def main():
   page_name = sidebar_component.write(PAGES)
-  
-  st.title("Common content for pages")
-
   page = PAGES[page_name]
-
   page.write()
 
 if __name__ == "__main__":
---
+++ components/menu.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def write():
+  st.title("This is main menu")
+
+  st.text_input("To jest text input")
+  # first_clicked = st.button("First task")
+  # second_clicked = st.button("Second task")
+  # third_clicked = st.button("Third task")
+
+  # if first_clicked:
+  #   return 1
+  # elif second_clicked:
+  #   return 2
+  # elif third_clicked:
+  #   return 3
+  
+  return 0
