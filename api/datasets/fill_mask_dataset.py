a3b52205d15130967b3b172da2b41f156e97ff95
Lukasz <poweska@student.agh.edu.pl>
2021-05-25 12:33:28 +0200

    Implemented mechanism to search description of fill mask's dataset

--- api/datasets/fill_mask_dataset.py
+++ api/datasets/fill_mask_dataset.py
@@ -1,29 +1,38 @@
-from datasets import load_dataset
+from datasets import load_dataset, list_datasets
 from dataset import Dataset
 from task_type import TaskType
 
 
 class FillMaskDatasets:
     def __init__(self):
-        self.datasets = self.__init_datasets()
+        self._datasets = self.__init_datasets()
+
+    def get_datasets(self):
+        return self._datasets
 
     @staticmethod
     def __init_datasets():
-        datasets = dict()
-        name = 'numer_sense'
-        description = 'NumerSense is a new numerical commonsense reasoning probing task, ' \
-                      'with a diagnostic dataset consisting of 3,145 masked-word-prediction probes.' \
-                      ' The general idea is to mask numbers between 0-10 in sentences mined from a commonsense' \
-                      ' corpus and evaluate whether a language model can correctly predict the masked value'
+        all_datasets = list_datasets(with_community_datasets=True, with_details=True)
+        fill_mask_datasets = dict()
 
-        datasets[name] = Dataset(name, description, TaskType.FILL_MASK)
-        return datasets
+        # TODO : jak przeflitrowac uzyskane zbiory danych po kategorii zadania
+
+        # Narazie nie wiem jak przefiltrować zbiory uzyskane z bilbioteki datasets po kategorii zadania.
+        # Narazie nazwy są ustawione na sztywno.
+
+        datasets_name = [
+            'numer_sense',
+            'kilt_tasks'
+        ]
+
+        for dataset in all_datasets:
+            if dataset.id in datasets_name:
+                fill_mask_datasets[dataset.id] = Dataset(dataset.id, dataset.description, TaskType.FILL_MASK)
+
+        return fill_mask_datasets
 
     def download_dataset(self, name):
-        if self.datasets[name] is None:
+        if self._datasets[name] is None:
             raise Exception("Dataset name is incorrect.")
 
         return load_dataset(name)
-
-
-
