4d4dedfef84f67221182bb45488aaa1739c09433
hardkov <krzysztofh150@gmail.com>
2021-11-08 22:40:11 +0100

    generic evaluation view

--- nlapp/view/pages/evaluation_page.py
+++ nlapp/view/pages/evaluation_page.py
@@ -3,7 +3,7 @@ import streamlit as st
 import nlapp.view.components.dataset.choose_dataset as choose_dataset
 import nlapp.view.components.choose_model as choose_model
 import nlapp.view.components.choose_task as choose_task
-import nlapp.view.components.evaluation as evaluation
+import nlapp.view.components.evaluation.evaluation as evaluation
 
 
 def write():
