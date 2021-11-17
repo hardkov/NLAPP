864787c96b643907237f0072a91ac04582ce80f7
hardkov <krzysztofh150@gmail.com>
2021-11-17 23:31:06 +0100

    fixed error with summarization models not being cached

--- nlapp/controller/app_controller.py
+++ nlapp/controller/app_controller.py
@@ -126,7 +126,7 @@ def get_dataset_dto(task_type: TaskType, dataset_name: str) -> DatasetDTO:
 
 
 @st.cache(
-    hash_funcs={"tokenizers.Tokenizer": id, "tokenizers.AddedToken": id},
+    hash_funcs={"tokenizers.Tokenizer": id, "tokenizers.AddedToken": id, "torch.nn.parameter.Parameter": id},
     max_entries=1,
 )
 def download_model(task_type: TaskType, name: str):
