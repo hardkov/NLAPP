b8694ee35a525b02f8e93c1e10982dc8588aa310
Grzegorz Janosz <gregory.janosz@gmail.com>
2021-11-21 23:41:03 +0100

    Change slightly conllu file view and refactor

--- nlapp/view/components/dataset/user_dataset_upload.py
+++ nlapp/view/components/dataset/user_dataset_upload.py
@@ -24,99 +24,81 @@ def get_json_string(file):
     return stringio.read()
 
 
-def display_json_mapper():
+def display_json_mapper(user_file):
     task = get_current_task()
-    user_file = st.session_state[KEYS.JSON_USER_DATASET_FILE]
-    if user_file is not None:
-        st.subheader("Map your dataset")
+    st.subheader("Map your dataset")
 
-        json_string = get_json_string(user_file)
-        st.json(json_string)
+    json_string = get_json_string(user_file)
+    st.json(json_string)
 
-        column_mapping = {}
-        columns = get_dataset_mapping_columns(task)
+    column_mapping = {}
+    columns = get_dataset_mapping_columns(task)
 
-        mapping_form = st.form("Mapping")
-        mapping_form.subheader("Mapping")
-        for column in columns:
-            mapping_form.markdown(f"Enter your mapping for **{column}** field")
-            column_mapping[column] = mapping_form.text_input(
-                "Your mapping", key=column
-            )
-        submitted = mapping_form.form_submit_button("Map")
-        if submitted:
-            if check_mapping_value_are_not_empty(column_mapping):
-                try:
-                    mapped = load_user_dataset(
-                        task_type=task,
-                        column_mapping=column_mapping,
-                        file_type=DatasetFormat.JSON,
-                        dataset=json.loads(json_string),
-                    )
-                except Exception as ex:
-                    st.session_state[KEYS.MAPPED_USER_DATASET] = None
-                    st.warning("Wrong mapping!")
-                else:
-                    st.session_state[KEYS.MAPPED_USER_DATASET] = mapped
-                    st.success("Mapped successfully.")
+    mapping_form = st.form("Mapping")
+    mapping_form.subheader("Mapping")
+    for column in columns:
+        mapping_form.markdown(f"Enter your mapping for **{column}** field")
+        column_mapping[column] = mapping_form.text_input(
+            "Your mapping", key=column
+        )
+    submitted = mapping_form.form_submit_button("Map")
+    if submitted:
+        if check_mapping_value_are_not_empty(column_mapping):
+            try:
+                mapped = load_user_dataset(
+                    task_type=task,
+                    column_mapping=column_mapping,
+                    file_type=DatasetFormat.JSON,
+                    dataset=json.loads(json_string),
+                )
+            except Exception as ex:
+                st.session_state[KEYS.MAPPED_USER_DATASET] = None
+                st.warning("Wrong mapping!")
             else:
-                st.error("You have to fill all fields!")
+                st.session_state[KEYS.MAPPED_USER_DATASET] = mapped
+                st.success("Mapped successfully.")
+        else:
+            st.error("You have to fill all fields!")
 
 
-def display_conllu_mapper():
-    user_file = st.session_state[KEYS.CONLLU_USER_DATASET_FILE]
-    if user_file is not None:
-        df = mapper.map_conllu_df(get_json_string(user_file))
-        st.dataframe(df, height=500)
+def display_conllu_mapper(user_file):
+    df = mapper.map_conllu_df(get_json_string(user_file))
+    st.dataframe(df, height=500)
 
 
-def display_mapper():
+def display_mapper(user_file):
     is_json = not st.session_state[KEYS.IS_USER_FILE_CONLLU]
     if is_json:
-        display_json_mapper()
+        display_json_mapper(user_file)
     else:
-        display_conllu_mapper()
+        display_conllu_mapper(user_file)
 
 
-def display_json_file_view():
-    file_uploader_description = "Allowed extensions: .json"
+def display_file_view(extension, file_key):
+    file_uploader_description = f"Allowed extensions: .{extension}"
+    st.session_state[KEYS.IS_USER_FILE_CONLLU] = extension != "json"
 
     user_file = st.file_uploader(
         file_uploader_description,
-        type=["json"],
-        key=KEYS.JSON_USER_DATASET_FILE,
+        type=[f"{extension}"],
+        key=file_key,
         accept_multiple_files=False,
     )
     if user_file is not None:
-        st.session_state[KEYS.IS_USER_FILE_CONLLU] = False
-
-
-def display_conllu_file_view():
-    file_uploader_description = "Allowed extensions: .conllu"
-
-    user_file = st.file_uploader(
-        file_uploader_description,
-        type=["conllu"],
-        key=KEYS.CONLLU_USER_DATASET_FILE,
-    )
-
-    if user_file is not None:
-        st.session_state[KEYS.IS_USER_FILE_CONLLU] = True
+        display_mapper(user_file)
 
 
 def write():
     task = get_current_task()
 
+    st.subheader("Upload file")
     if task.value is TaskType.TOKEN_CLASSIFICATION.value:
-        json_view, conllu_view = st.columns(2)
+        conllu_file = st.checkbox("Conllu files")
 
-        with json_view:
-            display_json_file_view()
-
-        with conllu_view:
-            display_conllu_file_view()
+        if conllu_file:
+            display_file_view("conllu", KEYS.CONLLU_USER_DATASET_FILE)
+        else:
+            display_file_view("json", KEYS.JSON_USER_DATASET_FILE)
 
     else:
-        display_json_file_view()
-
-    display_mapper()
+        display_file_view("json", KEYS.JSON_USER_DATASET_FILE)
